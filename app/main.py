e8faef9674a30741a86a2f3240252bb46eba2016
SCCSMARTCODE <sccsmart247@gmail.com>
2025-01-29 03:08:09 +0100

    completed app dev

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from datetime import datetime, UTC
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from typing import Dict
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/", response_model=Dict[str, str])
+def root():
+    return {
+          "email": os.getenv("MY_GMAIL"),
+          "current_datetime": datetime.now(UTC).isoformat(),
+          "github_url": os.getenv("PROJECT_GITHUB_URL")
+        }
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, log_level="info", reload=True)
